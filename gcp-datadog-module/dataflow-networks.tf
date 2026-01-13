4a5277a1d602b8aa4830a42cd0746e83cb918ac6
Nova Tan <ntan@wesdigital.com.au>
2026-01-13 17:03:54 +1100

    Comment out NAT resource to avoid conflict with existing VPC-wide NAT

--- gcp-datadog-module/dataflow-networks.tf
+++ gcp-datadog-module/dataflow-networks.tf
@@ -127,16 +127,17 @@ resource "google_compute_router" "dataflow_router" {
   region  = var.subnet_region
 }
 
-resource "google_compute_router_nat" "nat" {
-  project                            = var.project_id
-  name                               = "dataflow-machines-nat"
-  router                             = google_compute_router.dataflow_router.name
-  region                             = google_compute_router.dataflow_router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+# Commented out - using existing VPC-wide NAT instead
+# resource "google_compute_router_nat" "nat" {
+#   project                            = var.project_id
+#   name                               = "dataflow-machines-nat"
+#   router                             = google_compute_router.dataflow_router.name
+#   region                             = google_compute_router.dataflow_router.region
+#   nat_ip_allocate_option             = "AUTO_ONLY"
+#   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
+#   log_config {
+#     enable = true
+#     filter = "ERRORS_ONLY"
+#   }
+# }
